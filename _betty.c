6d1f7ac84a96f27d6515bb11f9eeaf7c4fe2702b
oussame-el1 <elhadraoui284@example.com>
2023-09-08 17:21:29 +0100

    _betty.c

---
+++ _betty.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+int main(int argc,char const **argv)
+{
+    int res , i;
+
+    const char *path = NULL;
+/*
+
+
+*/
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <command>\n", argv[0]);
+        exit(1);
+    }
+
+    if (strcmp(argv[1],"betty") == 0)
+    {
+        path = "/bin/betty";
+    }
+    res = execve(path, &argv[1], NULL);
+    if (res == -1)
+    {
+            perror("ERROR");
+            exit(1);
+    }
+return (0);
+}
